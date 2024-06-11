89c34b164dd98afaaae05b029f22bcbdd840d8a6
Francisco J. Casillas <chiccasillas@gmail.com>
2024-06-11 12:14:01 -0600

    Create TodoList component

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+function ToDoList() {
+	const [todos, setTodos] = useState([]);
+	const [newItem, setNewItem] = useState("");
+
+	const handleAddTodoItem = (event) => {
+		event.preventDefault;
+
+		const newTodoItem = {
+			id: Math.random().toString(36).substring(2, 9),
+			text: newItem,
+			completed: false,
+		};
+
+		setTodos([...todos, newTodoItem]);
+
+		setNewItem("");
+	};
+
+	return (
+		<div>
+			<h2>My To-do list</h2>
+			<input
+				type="text"
+				value={newItem}
+				placeholder="Enter new to-do item"
+				onChange={(event) => setNewItem(event.target.value)}
+			/>
+			<button onClick={(event) => handleAddTodoItem(event)}>Add item</button>
+			<ul>
+				{todos.map((todo) =>
+					todos.map((todo) => <li key={todo.id}>{todo.text}</li>)
+				)}
+			</ul>
+		</div>
+	);
+}
+
+export default ToDoList;
